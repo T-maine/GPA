0630934d08eb8d6fc06b3c60a872ee1ee6ed9457
T-maine <you@example.com>
2021-08-23 17:50:49 +0000

    Demo Commit

---
+++ one.py
@@ -0,0 +1,16 @@
+
+#member = [1,2,3,4,5,6]
+
+#member[2] = 4 
+#print(member)
+
+#name = input('name:')[4:9]
+#print(name)
+
+#kodua = 3 * '😂un' + 'ium'
+#print(kodua)
+
+numbers = [1,2,8,9]
+numbers[2:2] = [3,4,5,6,7,8]
+print(numbers)
+
---
+++ phil.py
@@ -0,0 +1,20 @@
+#school_jhs = [1,2,3,4,5,6,7,8]
+#primary = [11,13,13,12,24]
+#school_jhs.extend(primary)
+
+#print(school_jhs)
+
+#a = [1,2,3,4,5,6]
+#b = a.copy()
+#b[-1] = 9
+#print(a)
+#print(b)
+
+a = [1,2,3,4,5]
+a.clear()
+print(a)
+
+c = []
+b = input()
+c.append(b)
+print(c)
